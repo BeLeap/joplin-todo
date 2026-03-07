bc9e9c9af147e2880ec2fd3f56ac1f8900e5e648
Changseo Jang <beleap@beleap.dev>
2026-03-08 04:34:44 +0900

    Fix Android home widget rendering blank (#71)

---
+++ index.js
@@ -0,0 +1,5 @@
+const { registerJoplinHomeWidgetTask } = require('./src/features/widget/android-home-widget');
+
+registerJoplinHomeWidgetTask();
+
+require('expo-router/entry');
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,9 +4,6 @@ import React from 'react';
 import { useColorScheme } from 'react-native';
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import { registerJoplinHomeWidgetTask } from '@/features/widget/android-home-widget';
-
-registerJoplinHomeWidgetTask();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
