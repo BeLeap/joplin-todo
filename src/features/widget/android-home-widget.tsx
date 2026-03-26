f06fe20200dce9ae247b13f828636950acb530a0
Changseo Jang <beleap@beleap.dev>
2026-03-26 20:37:17 +0900

    Enable opening app from widget tap

--- src/features/widget/android-home-widget.tsx
+++ src/features/widget/android-home-widget.tsx
@@ -85,6 +85,7 @@ const WidgetRoot = ({ snapshot, explicitError }: { snapshot: WidgetSnapshot | nu
 
   return (
     <FlexWidget
+      clickAction="OPEN_APP"
       style={{
         height: 'match_parent',
         width: 'match_parent',
